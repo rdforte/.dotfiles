ee59d7ec4ac2095e7a78b4213ba530db77fd41dd
rf <rf@ryans-MacBook-Air.local>
2024-02-04 20:44:48 +1100

    update setup script to just install packages

--- setup-dev-machine.sh
+++ setup-dev-machine.sh
@@ -2,16 +2,10 @@
 
 set -e
 
-echo "🍺 => Installing XCode Developer tools..."
-xcode-select --install
-
 echo "🍺 => Installing GitHub CLI..."
 brew install gh
 
-echo "🍺 => Installing Brew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-echo "=> Installing Fonts..."
+echo "🍺 => Installing Fonts..."
 brew tap homebrew/cask-fonts
 brew install --cask font-jetbrains-mono-nerd-font
 
@@ -35,7 +29,6 @@ bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/bins
 echo "Delve"
 brew install delve
 
-
 echo "🍺 => Node Developer Tools..."
 echo "NVM"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
@@ -53,14 +46,6 @@ brew install --cask visual-studio-code
 # echo "🍺 => Arduino TinyGo Support..."
 # brew tap tinygo-org/tools
 # brew install tinygo
-# brew tap osx-cross/avr
-# brew install avr-gcc
 # brew install avrdude
-# git clone --recursive https://github.com/tinygo-org/tinygo.git
-# cd tinygo
-# git checkout dev
-# git submodule update --init
-# brew install llvm
-# go install
 
 echo "FINISHED!"
