54bb74337cc07280c51643f843766050ade77229
rf <rf@ryans-MacBook-Air.local>
2024-06-27 07:56:19 +1000

    renamed packages script and added protobuf

---
+++ mac-setup-script.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+set -e
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing Spectacle"
+brew install --cask spectacle # deprecated
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing GitHub CLI..."
+brew install gh
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing Hugo..."
+brew install hugo
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing Fonts..."
+brew alfred
+brew install --cask font-jetbrains-mono-nerd-font
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing Tmux..."
+brew install tmux
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing Kitty..."
+brew install kitty
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing NVIM..."
+brew install neovim
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing Ripgrep..."
+brew install ripgrep
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Go Developer Tools..."
+echo "GVM"
+brew update
+echo "Installing Mercurial"
+brew install mercurial
+echo "Installing Go so we can install GVM and set go version"
+brew install go
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+gvm install go1.22
+echo "Setting go1.22 as default"
+gvm use go1.22 --default
+echo "Uninstalling go"
+brew uninstall go
+echo "Installing Delve"
+brew install delve
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Node Developer Tools..."
+echo "NVM Node Version Manager"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing PYENV..."
+echo "Python Version manager"
+brew install pyenv
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing Protobuf..."
+brew install protobuf
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing Postman..."
+brew install --cask postman
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing Docker..."
+brew install docker
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing VSCode..."
+brew install --cask visual-studio-code
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing Slack..."
+brew install --cask slack
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing Notion"
+brew install --cask notion
+
+# image compression
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Installing GraphicsMagic..." 
+brew install graphicsmagick
+
+echo "🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩"
+echo "🍺 => Arduino TinyGo Support..."
+brew tap tinygo-org/tools
+brew install tinygo
+brew install avrdude
+
+echo "FINISHED! 👏"
